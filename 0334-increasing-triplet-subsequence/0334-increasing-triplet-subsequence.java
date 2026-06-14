d437866d2bacfee6dfda04280deb542ca98cb0fe
Arymankumar <arymankumar52@gmail.com>
2026-06-14 20:48:50 +0530

    Time: 2 ms (99.36%), Space: 122.6 MB (57.03%) - LeetHub

--- 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.java
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.java
@@ -3,7 +3,7 @@ class Solution {
         int n = nums.length;
         int left = Integer.MAX_VALUE;
         int mid = Integer.MAX_VALUE;
-        for (int num : nums) { //for(int i=0;i<n;i++)
+        for (int num : nums) { //for(int i=0;i<n;i++) and num[i]
             if (num <= left) {
                 left = num;
             } else if (num <= mid) {
