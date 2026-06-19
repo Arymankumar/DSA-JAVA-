9d84e28a3ac1752eac7f79966e89d31aceaf2817
Arymankumar <arymankumar52@gmail.com>
2026-06-19 18:15:31 +0530

    Time: 9 ms (5.4%), Space: 44.6 MB (8.13%) - LeetHub

---
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean uniqueOccurrences(int[] arr) {
+        int []count=new int[2001];
+        for(int i:arr){
+            count[i+1000]++;
+        }
+        Arrays.sort(count);
+        for(int i=0;i<2000;i++){
+            if(count[i] !=0 && count[i]==count[i+1]){
+                return false;
+            }
+        }
+        return true;
+    }
+}
